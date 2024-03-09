9eb7701056aee00a3b7db4ce73b422364691ab3a
Vaibhavi <135148044+Vaibhavi028@users.noreply.github.com>
2024-03-09 11:12:10 +0530

    updated

--- src/Signup.js
+++ src/Signup.js
@@ -18,17 +18,37 @@ const Signup = ({ onSignupSuccess }) => {
           <form onSubmit={handleSubmit}>
             <div className="signupform">
                 <div className="inputbox">
-                    <label htmlFor="name" class="lab1">Full Name</label>
+                    <label htmlFor="name" class="lab1">Name</label>
                     <input type="text" id="name" name="name" class="input1" />
                 </div>
-                <label htmlFor="email" class="lab1">VIT Email Address</label>
-                <input type="email" id="email" name="email" class="input1" />
-                <label htmlFor="phone" class="lab1">Phone Number</label>
-                <input type="tel" id="phone" name="phone" class="input1" />
-                <label htmlFor="password" class="lab1">Password</label>
-                <input type="password" id="password" class="input1" name="password" />
-                <label htmlFor="confirm-password" class="lab1">Confirm Password</label>
-                <input type="password" id="confirm-password" class="input1" name="confirm-password"/>
+                <div className="inputbox">
+                  <label htmlFor="city" class="lab1">City</label>
+                  <input type="text" id="city" name="city" class="input1" />
+                </div>
+                <div className="inputbox">
+                  <label htmlFor="state" class="lab1">State</label>
+                  <input type="text" id="state" name="state" class="input1" />
+                </div>
+                <div className="inputbox">
+                  <label htmlFor="pincode" class="lab1">Pin Code</label>
+                  <input type="number" id="pincode" name="pincode" class="input1" />
+                </div>
+                <div className="inputbox">
+                  <label htmlFor="phone" class="lab1">Phone</label>
+                  <input type="tel" id="phone" name="phone" class="input1" />
+                </div>
+                <div className="inputbox">
+                  <label htmlFor="email" class="lab1">Email</label>
+                  <input type="email" id="email" name="email" class="input1" />
+                </div>
+                <div className="inputbox">
+                  <label htmlFor="password" class="lab1">Password</label>
+                  <input type="password" id="password" class="input1" name="password" />
+                </div>
+                <div className="inputbox">
+                  <label htmlFor="confirm-password" class="lab1">Confirm Password</label>
+                  <input type="password" id="confirm-password" class="input1" name="confirm-password"/>
+                </div>
                 <div className="link-cont">
                     <a href="#" className="a1">Already have an account?  </a>
                     <a href="#" className="a2">Sign In</a>
